40d979da5e8a938adbbf688746b54d206cb547ba
david-ggs-230 <david.ggs@aliyun.com>
2025-03-01 09:37:07 +0800

    Add files via upload

---
+++ react-projects-with-typescript/tut02-writing-markup-with-jsx/src/App.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import "./App.css";
+import "./list-style.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const titleElement = <>The Rules of JSX</>;
+  const textContent= "JSX Expressions";
+  const classNames = "red-color bg-blue";
+  const person = {
+    name: 'George Bush',
+    theme: {
+      backgroundColor: 'black',
+      color: 'pink'
+    }
+  };
+  function handleClick() {
+    setCount((count) => count + 1);
+  }
+  return (
+    <div className="App listcontainer">
+      <h2>{titleElement}</h2>
+      <ol className="alternating-colors">
+        <li>
+          <strong>Return a single root element</strong>
+          <div>&quot;&lt;&gt;The Rules of JSX&lt;/&gt;&quot;</div>
+        </li>
+        <li>
+          <strong>Attributes in JSX</strong>
+          <div>
+            <div>&lt;div className="red-color"&gt;Red&lt;/div&gt;</div>
+            <div className="red-color">Red</div>
+          </div>
+        </li>
+        <li>
+          <strong>Event Handling</strong>
+          <div>
+            <button onClick={handleClick}>count is {count}</button>
+          </div>
+        </li>
+        <li>
+          <strong>JavaScript Expressions</strong>
+          <div>
+            <div>const classNames="red-color bg-blue"</div>
+            <div>
+              &lt;div className=&#123; classNames &#125;&gt;Red&lt;/div&gt;
+            </div>
+            <div className={classNames}>Red</div>
+            <div>const textContent= "JSX Expressions";</div>
+            <div>
+              &lt;div className="blue-color"&gt;&#123;textContent&#125;&lt;/div&gt;
+            </div>
+            <div className="blue-color">{textContent}</div>
+          </div>
+        </li>
+        <li>
+          <strong>JavaScript Objects and CSS</strong>
+          <div>
+            <div> &lt;div style=&#123;&#123;backgroundColor:'grey',color:'blue'&#125;&#125;&gt;...&lt;/div&gt;</div>
+            <div style={{ backgroundColor: 'grey', color: 'blue' }}>Grey with blue text</div>
+            <div>
+              &lt;div style=&#123;person.theme&#125;&gt;&#123;person.name&#125; &lt;/div&gt;
+            </div>
+            <div style={person.theme}>{person.name}</div>
+          </div>
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
