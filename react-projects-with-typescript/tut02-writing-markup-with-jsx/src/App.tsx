ff151a0743a49276561be03fe84df7686de8ef08
david-ggs-230 <david.ggs@aliyun.com>
2025-03-04 09:37:31 +0800

    Add files via upload

--- react-projects-with-typescript/tut02-writing-markup-with-jsx/src/App.tsx
+++ react-projects-with-typescript/tut02-writing-markup-with-jsx/src/App.tsx
@@ -18,60 +18,92 @@ function App() {
     setCount((count) => count + 1);
   }
   return (
-    <div className="App listcontainer">
+    <div className="list-container">
       <h2>{titleElement}</h2>
-      <ol className="alternating-colors">
-        <li>
-          <strong>Return a single root element</strong>
-          <div>&quot;&lt;&gt;The Rules of JSX&lt;/&gt;&quot;</div>
-        </li>
-        <li>
-          <strong>Attributes in JSX</strong>
-          <div>
-            <div>
-              &lt;div className=&quot;red-color&quot;&gt;Red&lt;/div&gt;
-            </div>
-            <div className="red-color">Red</div>
+      <ol>
+        <li className="list-item">
+          <div className="list-item-number"></div>
+          <div className="list-item-content">
+            <h3>Return a single root element</h3>
+            <p>&quot;&lt;&gt;The Rules of JSX&lt;/&gt;&quot;</p>
           </div>
         </li>
-        <li>
-          <strong>Event Handling</strong>
-          <div>
-            <button onClick={handleClick}>count is {count}</button>
+        <li className="list-item">
+          <div className="list-item-number"></div>
+          <div className="list-item-content">
+            <h3>Attributes in JSX</h3>
+            <p>&lt;p className=&quot;red-color&quot;&gt;Red&lt;/p&gt;</p>
+            <p className="red-color">Red</p>
           </div>
         </li>
-        <li>
-          <strong>JavaScript Expressions</strong>
-          <div>
-            <div>const classNames=&quot;red-color bg-blue&quot;</div>
-            <div>
-              &lt;div className=&#123; classNames &#125;&gt;Red&lt;/div&gt;
-            </div>
-            <div className={classNames}>Red</div>
-            <div>const textContent= &quot;JSX Expressions&quot;;</div>
-            <div>
-              &lt;div
-              className=&quot;blue-color&quot;&gt;&#123;textContent&#125;&lt;/div&gt;
-            </div>
-            <div className="blue-color">{textContent}</div>
+        <li className="list-item">
+          <div className="list-item-number"></div>
+          <div className="list-item-content">
+            <h3>Event Handling</h3>
+            <p>
+              <button onClick={handleClick}>count is {count}</button>
+            </p>
           </div>
         </li>
-        <li>
-          <strong>JavaScript Objects and CSS</strong>
-          <div>
-            <div>
-              {" "}
-              &lt;div
-              style=&#123;&#123;backgroundColor:&apos;grey&apos;,color:&apos;blue&apos;&#125;&#125;&gt;...&lt;/div&gt;
+        <li className="list-item">
+          <div className="list-item-number"></div>
+          <div className="list-item-content">
+            <h3>JavaScript Expressions: Attributes</h3>
+            <p>const classNames=&quot;red-color bg-blue&quot;</p>
+            <p>&lt;p className=&#123; classNames &#125;&gt;Red&lt;/p&gt;</p>
+            <p className={classNames}>Red</p>
+          </div>
+        </li>
+        <li className="list-item">
+          <div className="list-item-number"></div>
+          <div className="list-item-content">
+            <h3>JavaScript Expressions: Contents</h3>
+            <p>const textContent= &quot;JSX Expressions&quot;;</p>
+            <p>
+              &lt;p
+              className=&quot;blue-color&quot;&gt;&#123;textContent&#125;&lt;
+              /p&gt;
+            </p>
+            <p className="blue-color">{textContent}</p>
+          </div>
+        </li>
+        <li className="list-item">
+          <div className="list-item-number"></div>
+          <div className="list-item-content">
+            <h3>Inline CSS Styles</h3>
+            <p>
+              &lt;p style=&#123;&#123; backgroundColor: &quot;grey&quot;, color:
+              &quot;blue&quot;
+              <br /> &#125;&#125; &gt; Grey background with blue text &lt;
+              /p&gt;
+            </p>
+            <p style={{ backgroundColor: "grey", color: "blue" }}>
+              Grey background with blue text
+            </p>
+          </div>
+        </li>
+        <li className="list-item">
+          <div className="list-item-number"></div>
+          <div className="list-item-content">
+            <h3>JavaScript Objects</h3>
+            <div style={{ textAlign: "left" }}>
+              const person = &#123;
+              <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;name: &quot;George Bush&quot;,
+              <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;theme: &#123;
+              <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;backgroundColor: &quot;black&quot;,
+              <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;color: &quot;pink&quot;, &#125;
+              <br />
+              &#125;;
             </div>
-            <div style={{ backgroundColor: "grey", color: "blue" }}>
-              Grey with blue text
-            </div>
-            <div>
-              &lt;div style=&#123;person.theme&#125;&gt;&#123;person.name&#125;
-              &lt;/div&gt;
-            </div>
-            <div style={person.theme}>{person.name}</div>
+            <p>
+              &lt;p style=&#123;person.theme&#125;&gt;&#123;person.name&#125;
+              &lt;/p&gt;
+            </p>
+            <p style={person.theme}>{person.name}</p>
           </div>
         </li>
       </ol>
