2a4dae1fc03ea376abd683af213ccedf2ec765c5
david-ggs-230 <david.ggs@aliyun.com>
2025-03-13 11:06:49 +0800

    Delete react-projects-with-typescript/tut05-function-components-props directory

--- react-projects-with-typescript/tut05-function-components-props/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import FunctionComponentsDisplay from "./FunctionComponentDisplay";
-import "./App.css";
-
-function App() {
-  return <FunctionComponentsDisplay />;
-}
-
-export default App;
--- react-projects-with-typescript/tut05-function-components-props/src/Person.tsx
+++
@@ -1,7 +0,0 @@
-type Person = {
-  name?: string;
-  age?: number;
-  location?: string;
-};
-
-export default Person;
