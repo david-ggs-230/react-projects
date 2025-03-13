f1634e785227cf12818fc71f309074a2c2f5ddd8
david-ggs-230 <david.ggs@aliyun.com>
2025-03-13 10:35:07 +0800

    Delete react-projects-with-typescript/tut05-function-components-event directory

--- react-projects-with-typescript/tut05-function-components-event/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import FunctionComponentsDisplay from "./FunctionComponentsDisplay";
-import "./App.css";
-import "./list-style.css";
-
-function App() {
-  return <FunctionComponentsDisplay />;
-}
-
-export default App;
